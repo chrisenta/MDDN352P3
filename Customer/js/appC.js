f700517a5e8465c2b8425f2b836e4d9d350f6cc3
Christine Ling <jernsze@hotmail.com>
2016-05-23 12:21:16 +1200

    Merged conflict

--- Customer/js/appC.js
+++ Customer/js/appC.js
@@ -9,35 +9,6 @@
 
   var data = firebase.database().ref();
 
-<<<<<<< Updated upstream
-	data.on("value", function(snapshot) {
-		var context = snapshot.val();
-  		console.log(context);
-	});
-
-	$("#submit").click(function(){
-		var coffee = {
-		size: null,
-		caffeine: null,
-		shots: null,
-		sugar: null,
-		payment: null,
-		collection: null,
-		status: null,
-		}
-
-	coffee.size = $('input:radio[name=size]:checked').val();
-	coffee.caffeine = $('input:radio[name=caffeine]:checked').val();
-	coffee.shots = $('input:radio[name=shots]:checked').val();
-	coffee.sugar = $('input:radio[name=sugar]:checked').val();
-	coffee.payment = $('input:radio[name=cc]:checked').val();
-	coffee.collection = $('input:radio[name=collection]:checked').val();
-	coffee.status = "Waiting in Line";
-
-	console.log(coffee);
-	data.child("coffees").push(coffee);
-	});
-=======
 data.on("value", function(snapshot) {
 	var context = snapshot.val();
 		console.log(context);
@@ -80,5 +51,4 @@ console.log(coffee);
 data.child("coffees").push(coffee);
 });
 
->>>>>>> Stashed changes
 
