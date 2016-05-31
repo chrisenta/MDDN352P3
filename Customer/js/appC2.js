9ba5f264e1390935bc51f1c0fda8571700e37dc8
Christine Ling <jernsze@hotmail.com>
2016-06-01 00:32:15 +1200

    Tidying Code

--- Customer/js/appC2.js
+++ Customer/js/appC2.js
@@ -21,8 +21,7 @@
 		 $("#yield").html(html);
 	});
 
-
-
+//Cropbox plugin
 
 $('.cropimage').cropbox({
   width: 500,
@@ -33,26 +32,14 @@ $('.cropimage').cropbox({
 });
 
 
-
-     // When the document loads do everything inside here ...
-     $(document).ready(function(){
-        
-       // When a link is clicked
-       $("a.tab").click(function () {
-            
-           // switch all tabs off
-           $(".active").removeClass("active");
-            
-           // switch this tab on
+//Map tabs
+$(document).ready(function(){
+    $("a.tab").click(function () {
+        $(".active").removeClass("active");
            $(this).addClass("active");
-            
-           // slide all elements with the class 'content' up
            $(".content").slideUp();
-            
-           // Now figure out what the 'title' attribute value is and find the element with that id.  Then slide that down.
+
            var content_show = $(this).attr("title");
            $("#"+content_show).slideDown();
-          
-       });
-    
-     });
+    });
+});
