7375dcbea33c7a680a0bd878d358e2a253295efe
Christine Ling <jernsze@hotmail.com>
2016-05-31 23:20:27 +1200

    Trying out cropbox

--- Customer/js/appC2.js
+++ Customer/js/appC2.js
@@ -22,3 +22,35 @@
 	});
 
 
+
+    $( function () {
+      $( '.cropimage' ).each( function () {
+        var image = $(this),
+            cropwidth = image.attr('cropwidth'),
+            cropheight = image.attr('cropheight'),
+            results = image.next('.results' ),
+            x       = $('.cropX', results),
+            y       = $('.cropY', results),
+            w       = $('.cropW', results),
+            h       = $('.cropH', results),
+            download = results.next('.download').find('a');
+
+          image.cropbox( {width: cropwidth, height: cropheight, showControls: 'auto' } )
+            .on('cropbox', function( event, results, img ) {
+              x.text( results.cropX );
+              y.text( results.cropY );
+              w.text( results.cropW );
+              h.text( results.cropH );
+              download.attr('href', img.getDataURL());
+            });
+      } );
+
+      $('#select').on('change', function () {
+          var size = parseInt(this.value);
+          $('.cropimage').each(function () {
+            $(this).cropbox({width: size, height: size})
+          });
+      });
+
+    } );
+  
